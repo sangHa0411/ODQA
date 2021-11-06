274ced1e23c77671db23b5fed225a3e928a82aec
sangha <sangha0411@gmail.com>
2021-11-06 13:26:27 +0000

    add baseline code

---
+++ install_requirements.sh
@@ -0,0 +1,8 @@
+pip install datasets==1.4.0
+pip install transformers==4.11.0
+pip install torch==1.6.0
+pip install tqdm==4.41.1
+pip install pandas==1.1.4
+pip install scikit-learn==0.24.1
+pip install konlpy==0.5.2
+pip install faiss-gpu
